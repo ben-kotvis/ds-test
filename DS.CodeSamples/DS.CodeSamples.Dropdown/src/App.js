a58549ab1feb1211c4110c01b2422b25ef0793e4
Seth Broweleit <sbroweleit@bluemetal.com>
2018-09-05 17:04:02 -0400

    syncfusion dropdown

--- DS.CodeSamples/DS.CodeSamples.Dropdown/src/App.js
+++ DS.CodeSamples/DS.CodeSamples.Dropdown/src/App.js
@@ -1,16 +1,40 @@
 import React, { Component } from "react";
 
+import { DropDownListComponent } from "@syncfusion/ej2-react-dropdowns";
+
 class App extends Component {
+  constructor() {
+    super(...arguments);
+    // define the JSON of data
+    this.sportsData = [
+      { Id: "Game1", Game: "American Football" },
+      { Id: "Game2", Game: "Badminton" },
+      { Id: "Game3", Game: "Basketball" },
+      { Id: "Game4", Game: "Cricket" },
+      { Id: "Game5", Game: "Football" },
+      { Id: "Game6", Game: "Golf" },
+      { Id: "Game7", Game: "Hockey" },
+      { Id: "Game8", Game: "Rugby" },
+      { Id: "Game9", Game: "Snooker" },
+      { Id: "Game10", Game: "Tennis" }
+    ];
+    // maps the appropriate column to fields property
+    this.fields = { text: "Game", value: "Id" };
+    // set the value to select an item based on mapped value at initial rendering
+    this.value = "Game3";
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="m-8">
+        <h1>DropDown</h1>
+        <br />
+        <br />
+        <DropDownListComponent
+          placeholder="Select a thing"
+          dataSource={this.sportsData}
+          fields={this.fields}
+          value={this.value}
+        />
       </div>
     );
   }

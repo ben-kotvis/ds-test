94b78acb53e71553df68a5a4b6a5ab69d003ee30
Seth Broweleit <sbroweleit@bluemetal.com>
2018-09-05 14:16:15 -0400

    uneject

--- DS.CodeSamples/DS.CodeSamples.Checkbox/src/index.js
+++ DS.CodeSamples/DS.CodeSamples.Checkbox/src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "../node_modules/bm-malt-css/preflight.css";
-import "../node_modules/bm-malt-css/kendo-themed.css";
-import "../node_modules/bm-malt-css/utils.css";
+import "./index.css";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));

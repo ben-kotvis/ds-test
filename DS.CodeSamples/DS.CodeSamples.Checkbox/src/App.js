9f82046476c97a2f218a5d68ba5c0b408a3818b9
Seth Broweleit <sbroweleit@bluemetal.com>
2018-09-05 10:31:58 -0400

    tweaks

--- DS.CodeSamples/DS.CodeSamples.Checkbox/src/App.js
+++ DS.CodeSamples/DS.CodeSamples.Checkbox/src/App.js
@@ -3,28 +3,24 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div className="k-ripple-container">
-        <h1>Checkboxes</h1>
-        <input className="k-checkbox" id="checkbox1" type="checkbox" />
-        <label className="k-checkbox-label" htmlFor="checkbox1">
-          Android
-        </label>
-        <span>&nbsp;</span>
-        <span>&nbsp;</span>
-        <span>&nbsp;</span>
+      <div className="k-form">
+        <div className="k-form-field">
+          <h1>Checkboxes</h1>
+          <input className="k-checkbox" id="checkbox1" type="checkbox" />
+          <label className="k-checkbox-label" htmlFor="checkbox1">
+            Checkbox 1
+          </label>
 
-        <input className="k-checkbox" id="checkbox2" type="checkbox" />
-        <label className="k-checkbox-label" htmlFor="checkbox2">
-          iOS
-        </label>
-        <span>&nbsp;</span>
-        <span>&nbsp;</span>
-        <span>&nbsp;</span>
+          <input className="k-checkbox" id="checkbox2" type="checkbox" />
+          <label className="k-checkbox-label" htmlFor="checkbox2">
+            Checkbox 2
+          </label>
 
-        <input className="k-checkbox" id="checkbox3" type="checkbox" />
-        <label className="k-checkbox-label" htmlFor="checkbox3">
-          Windows Phone
-        </label>
+          <input className="k-checkbox" id="checkbox3" type="checkbox" />
+          <label className="k-checkbox-label" htmlFor="checkbox3">
+            Checkbox 3
+          </label>
+        </div>
       </div>
     );
   }
--- DS.CodeSamples/DS.CodeSamples.Radio/src/App.js
+++ DS.CodeSamples/DS.CodeSamples.Radio/src/App.js
@@ -3,29 +3,23 @@ import React, { Component } from "react";
 class App extends Component {
   render() {
     return (
-      <div className="k-ripple-container">
-        <h1>RadioButtons</h1>
-        <input className="k-radio" id="radio1" name="radios" type="radio" />
-        <label className="k-radio-label" htmlFor="radio1">
-          Android
-        </label>
-        <span>&nbsp;</span>
-        <span>&nbsp;</span>
-        <span>&nbsp;</span>
-
-        <input className="k-radio" id="radio2" name="radios" type="radio" />
-        <label className="k-radio-label" htmlFor="radio2">
-          iOS
-        </label>
-        <span>&nbsp;</span>
-        <span>&nbsp;</span>
-        <span>&nbsp;</span>
-
-        <input className="k-radio" id="radio3" name="radios" type="radio" />
-        <label className="k-radio-label" htmlFor="radio3">
-          Windows Phone
-        </label>
-      </div>
+      <form className="k-form">
+        <div className="k-form-field">
+          <h1>RadioButtons</h1>
+          <input className="k-radio" id="radio1" name="radios" type="radio" />
+          <label className="k-radio-label" htmlFor="radio1">
+            RadioButton 1
+          </label>
+          <input className="k-radio" id="radio2" name="radios" type="radio" />
+          <label className="k-radio-label" htmlFor="radio2">
+            RadioButton 2
+          </label>
+          <input className="k-radio" id="radio3" name="radios" type="radio" />
+          <label className="k-radio-label" htmlFor="radio3">
+            RadioButton 3
+          </label>
+        </div>
+      </form>
     );
   }
 }

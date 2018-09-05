86ede2b8717fe6d8262cdb392eaa3ad5ce62d010
Seth Broweleit <sbroweleit@bluemetal.com>
2018-09-05 14:09:01 -0400

    node css without ejecting

---
+++ DS.CodeSamples/DS.CodeSamples.Textarea/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-title">TextArea</h1>
+        <textarea className="k-textarea" />
+      </div>
+    );
+  }
+}
+
+export default App;
